3fda4ba31067b692cf7b43a77f7fa97d9fa1a13e
trianandafajar <mail.trianandafajar@gmail.com>
2025-03-14 11:28:37 +0700

    Update Reducer

--- 02-react-hook/src/reducers/kontak/index.jsx
+++ 02-react-hook/src/reducers/kontak/index.jsx
@@ -1,6 +1,15 @@
-const kontak = (state, action) => {
-  const { type } = action;
+const initialState = [];
+
+const kontak = (state = initialState, action) => {
+  const { type, payload } = action;
+
   switch (type) {
+    case "ADD_KONTAK":
+      return [...state, payload];
+
+    case "REMOVE_KONTAK":
+      return state.filter((kontak) => kontak.id !== payload);
+
     default:
       return state;
   }
